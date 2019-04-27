944b54a49f6c4e33bfbb6d68969d3b110a98ce42
xaviguasch <xaviguasch@gmail.com>
2019-04-27 16:31:46 +0200

    toyed with basic props

--- Hello.js
+++ Hello.js
@@ -1,10 +1,12 @@
 class Hello extends React.Component {
   render() {
+    let bangs = '!'.repeat(this.props.bangs)
     return (
       <div>
         <p>
-          Hi {this.props.to} from {this.props.from}
+          Hi {this.props.to} from {this.props.from} {bangs}
         </p>
+        <img src={this.props.img} alt='' />
       </div>
     )
   }
--- index.js
+++ index.js
@@ -2,8 +2,16 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello to='Ringo' from='Paul' />
-        <Hello to='Cher' from='Sonny' />
+        <Hello
+          to='Ringo'
+          from='Paul'
+          num={3}
+          data={[1, 2, 3, 4, 5]}
+          bangs={4}
+          img={
+            'https://images.unsplash.com/photo-1556034910-766bb954e414?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'
+          }
+        />
       </div>
     )
   }
